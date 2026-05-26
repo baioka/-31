e844026bc2e891677828400d161ee5df114e9fa0
baioka <bajarystanhb@gmail.com>
2026-05-26 20:09:32 +0500

    Update PersonComparator.java

--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -3,7 +3,19 @@ import java.util.Comparator;
 public class PersonComparator implements Comparator<Person> {
     @Override
     public int compare(Person o1, Person o2) {
-        // implement this according to your variant
-        return 0;
+        int last = o1.lastname.compareTo(o2.lastname);
+int last = o1.lastname.compareTo(o2.lastname);
+
+if (last != 0) {
+    return last;
+}
+
+int first = o1.firstname.compareTo(o2.firstname);
+
+if (first != 0) {
+    return first;
+}
+
+return o1.weight - o2.weight;
     }
 }
