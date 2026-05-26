61979ac37cf9afdbfd853d6d84abdee4941bc4c6
baioka <bajarystanhb@gmail.com>
2026-05-26 20:03:11 +0500

    Update ElementarySort.java

--- src/ElementarySort.java
+++ src/ElementarySort.java
@@ -3,12 +3,42 @@ import java.util.Comparator;
 public class ElementarySort {
     // bubble sort variant
     public static <E extends Comparable<E>> void sort(E[] list){
-        // implement it according to your variant
-    }
 
-    public static <E> void sort(E[] list, Comparator<? super E> comparator) {
-        // implement it according to your variant
+    for (int i = 0; i < list.length - 1; i++) {
+
+        int minIndex = i;
+
+        for (int j = i + 1; j < list.length; j++) {
+
+            if (list[j].compareTo(list[minIndex]) < 0) {
+                minIndex = j;
+            }
+        }
+
+        E temp = list[i];
+        list[i] = list[minIndex];
+        list[minIndex] = temp;
     }
+}
+
+public static <E> void sort(E[] list, Comparator<? super E> comparator) {
+
+    for (int i = 0; i < list.length - 1; i++) {
+
+        int minIndex = i;
+
+        for (int j = i + 1; j < list.length; j++) {
+
+            if (comparator.compare(list[j], list[minIndex]) < 0) {
+                minIndex = j;
+            }
+        }
+
+        E temp = list[i];
+        list[i] = list[minIndex];
+        list[minIndex] = temp;
+    }
+}
 
     // Example for Comparable usage
     public static <E extends Comparable<E>> E findMin(E[] list){
